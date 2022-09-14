5dff6876dc42101df543b1ad1cd6e4902b6d8a4b
quyet tran <tranngocquyet1999@gmail.com>
2022-09-14 15:10:46 +0700

    firstcommit

---
+++ GettingStart/GettingStart/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GettingStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello.");
+            Console.Write("Please enter your name: ");
+            string x = Console.ReadLine();
+            Console.WriteLine("Hello "+ x);
+        }
+    }
+}
