fda92aa15f76ac09e0f3e9d3a077429c9cbcd781
Quyettn1 <113501848+Quyettn1@users.noreply.github.com>
2022-09-19 14:49:30 +0700

    Add files via upload

---
+++ Net.M.A004 .Exercise2/NPL.M.A001.Exercise1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NPL.M.A001.Exercise1
+{
+    class Program
+    {
+        // y = 2x3 – 6x2 + 2x -1
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Giai phuong trinh : y = 2x3 - 6x2 + 2x -1");            
+            int x;
+            do
+            {
+                Console.Write("nhap x = ");
+            }
+            while (!int.TryParse(Console.ReadLine(), out x));            
+            Console.WriteLine(" => y = "+ (2*x*x*x - 6*x*x +2*x - 1).ToString());
+            Console.ReadLine();
+        }
+    }
+}
